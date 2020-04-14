328db85d5ba33850c30fc972721ceb5b7672af27
Madoxen <pchlad5@gmail.com>
2020-04-14 03:40:06 +0200

    Project layout

---
+++ FilterLib/IFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FilterLib
+{
+
+    /// <summary>
+    /// Interface that describes generic filter
+    /// Methods, properties TBD
+    /// </summary>
+    public interface IFilter
+    { 
+    
+    }
+}
