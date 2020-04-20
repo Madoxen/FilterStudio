feffb921392a0ac9e38ba70af5eff23bb36772aa
Madoxen <pchlad5@gmail.com>
2020-04-20 19:51:13 +0200

    Added simple null check tests

---
+++ FilterStudio.Tests/ExecutionVMTests.cs
@@ -0,0 +1,22 @@
+﻿using FilterStudio.VM;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilterStudio.Tests
+{
+    [TestClass]
+    public class ExecutionVMTests
+    {
+        [TestMethod]
+        public void TestForNullValuesWithDefaultCtor()
+        {
+            ExecutionEngineVM vm = new ExecutionEngineVM(new System.Collections.ObjectModel.ObservableCollection<FilterVM>());
+            Assert.IsNotNull(vm.ExecuteTreeCommand);
+            Assert.IsNotNull(vm.LoadImageCommand);
+            Assert.IsNotNull(vm.SaveImageCommand);
+
+        }
+    }
+}
