2927bbfc713a2cb13f79489356847e0c308b7975
Madoxen <pchlad5@gmail.com>
2020-04-20 15:03:34 +0200

    Idea revamp to meet university deadlines

---
+++ FilterStudio/Core/ExecutionEngine.cs
@@ -0,0 +1,50 @@
+﻿using FilterStudio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilterStudio.Core
+{
+    /// <summary>
+    /// Engine that manages execution of Operation Tree
+    /// </summary>
+    public class ExecutionEngine
+    {
+        List<IFilter> filters = new List<IFilter>();
+        public List<IFilter> Filter
+        {
+            get { return filters; }
+        }
+
+
+
+        /// <summary>
+        /// Default constructor, will create empty Filters list
+        /// </summary>
+        public ExecutionEngine()
+        { 
+        }
+
+
+        /// <summary>
+        /// Constructor that will accept and create NEW instance of provided filter list
+        /// </summary>
+        /// <param name="filters"></param>
+        public ExecutionEngine(List<IFilter> filters)
+        {
+            filters = new List<IFilter>(filters);
+        }
+
+
+        /// <summary>
+        /// Executes filters from top to bottom
+        /// </summary>
+        public void Execute()
+        {
+            foreach (IFilter filter in filters)
+            {
+                filter.Operate();
+            }
+        }
+    }
+}
---
+++ FilterStudio/Interfaces/IFilter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace FilterStudio.Interfaces
+{
+    /// <summary>
+    /// Represents an 
+    /// </summary>
+    public interface IFilter
+    {
+        
+
+        /// <summary>
+        /// Layer that this filter is in. Values of subsequent images are pushed from 0 -> Max 
+        /// </summary>
+        int Layer { get; set; }
+
+
+        /// <summary>
+        /// An Operation Method that will execute code in concrete implementation
+        /// using Inputs provided values and after execution, will push necessary values to outputs
+        /// </summary>
+        void Operate();
+
+        /// <summary>
+        /// Value that will be pushed after Operate 
+        /// </summary>
+        Image Output { get; }
+
+        /// <summary>
+        /// Value that is pushed before Operate
+        /// </summary>
+        Image Input { get; set; }
+    }
+}
