a62e8c4c1ea83ebc46ade1f505259660b0bcb866
Madoxen <pchlad5@gmail.com>
2020-06-28 03:23:05 +0200

    Added preset system

---
+++ FilterStudio/Core/FilterFactoryRegister.cs
@@ -0,0 +1,20 @@
+﻿using FilterStudio.Concrete.FilterFactories;
+using FilterStudio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilterStudio.Core
+{
+    /// <summary>
+    /// Class that holds all filter factories with their respective name identifiers
+    /// </summary>
+    public static class FilterFactoryRegister
+    {
+        public readonly static Dictionary<string, IFilterFactory> Factories = new Dictionary<string, IFilterFactory>()
+        {
+            ["BasicFilter"] = new BasicFilterFactory(),
+            ["GaussianFilter"] = new GaussianFilterFactory(),
+        };
+    }
+}
